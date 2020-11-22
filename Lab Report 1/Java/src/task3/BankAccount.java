7014311e56401be541ce819d7432595be3208610
mohsin-code <mohsinshah3171@gmail.com>
2020-11-22 23:43:00 +0500

    Create task3 Bank Account class

---
+++ Lab Report 1/Java/src/task3/BankAccount.java
@@ -0,0 +1,24 @@
+package task3;
+
+public class BankAccount {
+	private int balance;
+	
+	public BankAccount() {
+		balance = 1000;
+	}
+	
+	public void deposit(int amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(int amount) {
+		if(balance <= 500)
+			System.out.println("Insufficient Balance!");
+		else
+			balance -= amount;
+	}
+	
+	public void display() {
+		System.out.println("\nCurrent Balance: " + balance);
+	}
+}
