16fa03e445eab89ec981dccda806722b733b9819
mohsin-code <mohsinshah3171@gmail.com>
2020-11-22 20:02:54 +0500

    Make the h2 Heater to below freezing

--- Lab Report 1/C++/Heater(Task-1).cpp
+++ Lab Report 1/C++/Heater(Task-1).cpp
@@ -28,12 +28,19 @@ main()
     Heater h1;      //Create h1 Heater
 
     h1.getTemp();   //Get h1's temprature
-    h1.cooler();    //h1 Heater is cooled
+    h1.warmer();    //h1 Heater is cooled
     h1.getTemp();   //get h1's temprature after cooling
+    h1.cooler();
+    h1.getTemp();
 
     Heater h2;      //Create h2 Heater
 
     h2.getTemp();   //get  h2's temprature
-    h2.warmer();    //h2 Heater is warmed
-    h2.getTemp();   //get h2 Heater's temprature after heating
+    h2.cooler();    //h2 Heater is warmed
+    h2.cooler();
+    h2.cooler();
+    h2.cooler();
+    h2.cooler();
+    h2.getTemp();   //get h2 Heater's temprature after cooling
+    //Oh it is below freezing
 }
