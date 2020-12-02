98418df5b14d669ff41f1f9be7c24416b8a5e349
mohsin-code <mohsinshah3171@gmail.com>
2020-12-02 21:51:34 +0500

    Create Task 2 in C++

---
+++ Lab Report 2/Task2.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+class complex {
+    private:
+        double re, im;
+    public:
+        complex();
+        complex(double r, double i);
+        // complex negate();       //returns complex object
+        void show();
+        void input();
+        complex addCom(complex c);
+        complex subCom(complex c);
+        complex mulCom(complex c);
+};
+
+complex::complex():re(0),im(0){}
+
+complex::complex(double r, double i):re(r),im(i){}
+
+// complex complex::negate() {
+//     complex temp;
+//     temp.re= - re;
+//     temp.im= - im;
+//     return temp;
+// }
+
+void complex::show() {
+    if(im>0)
+        cout << re << " + " << im << "i" << endl;
+    else
+        cout << re << im << "i" << endl;
+}
+
+void complex::input() {
+    cout<<"\nEnter real number: ";
+    cin>>re;
+    cout<<"\nEnter imaginary number: ";
+    cin>>im;
+}
+
+complex complex::addCom(complex c) {
+    complex x;
+    x.re = re + c.re;
+    x.im = im + c.im;
+    return x;    
+}
+
+complex complex::subCom(complex c) {
+    complex x;
+    x.re = re - c.re;
+    x.im = im - c.im;
+    return x;    
+}
+
+complex complex::mulCom(complex c) {
+    complex x;
+    x.re = (re * c.re) - (im * c.im);
+    x.im = (re * c.im) + (c.re * im);
+}
+
+main() {
+    complex c1,c2,c;
+    
+    c1.input();
+    c1.show();
+
+    c2.input();
+    c2.show();
+
+    c = c1.addCom(c2);
+    cout<<"Sum = ";
+    c.show();
+
+    c = c1.subCom(c2);
+    cout<<"Difference = ";
+    c.show();
+
+    c = c1.mulCom(c2);
+    cout<<"Product = ";
+    c.show();
+}
