ba3678477e4e68cb1509929db8b02c771ea2f662
mohsin-code <mohsinshah3171@gmail.com>
2020-12-26 02:06:21 +0500

    Create driver function

--- Lab Report 4/C++/Task1.cpp
+++ Lab Report 4/C++/Task1.cpp
@@ -26,9 +26,19 @@ class Employee
             serviceTime = t;
         }
 
+        //Deep Copy Constructor
         Employee(const Employee &e)
         {
-            //Deep copy of data members
+            int length = strlen(e.name);
+            name = new char[length+1];
+            strcpy(name, e.name);
+
+            length = strlen(e.department);
+            department = new char[length+1];
+            strcpy(department, e.department);
+
+            salary = e.salary;
+            serviceTime = e.serviceTime;
         }
 
         void input()
@@ -42,9 +52,33 @@ class Employee
             cout<<"Enter time of service:";
             cin>>serviceTime;
         }
+        
+        void show()
+        {
+            cout<<"\nDetails:\nName: "<<name<<endl;
+            cout<<"Enter department: "<<department<<endl;
+            cout<<"Enter salary:"<<salary<<endl;
+            cout<<"Enter time of service:"<<serviceTime<<endl;
+        }
 
         ~Employee()
         {
             delete name, department, salary, serviceTime;
         }
+};
+
+int main()
+{
+    Employee e1;
+
+    e1.input();
+    e1.show();
+
+    Employee e2 = e1;
+
+    e2.show();
+
+    Employee e3(e1);
+    
+    e3.show();
 }
